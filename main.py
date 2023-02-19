de1b77861dad9c4c389454c7365b1161ba231eee
221RDB270 <125795302+221RDB270@users.noreply.github.com>
2023-02-19 18:14:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def find_mismatch(text):
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack.pop().char, next):
                 return i+1
             opening_brackets_stack.pop()
             pass
@@ -36,5 +36,4 @@ def main():
 
 
 if __name__ == "__main__":
-    input("I")
     main()
