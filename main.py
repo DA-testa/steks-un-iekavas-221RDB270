2ad3b1b4facab7146ae47c4a5ed2e9f3a635e9c1
221RDB270 <125795302+221RDB270@users.noreply.github.com>
2023-02-19 18:09:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ def find_mismatch(text):
         if next in "([{":
             # Process opening bracket, write your code here
             opening_brackets_stack.append(Bracket(next, i + 1))   
-             
+            pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
             opening_brackets_stack.pop()
-            
+            pass
         if opening_brackets_stack:
             return opening_brackets_stack[0].position 
         return "Success"   
@@ -36,4 +36,5 @@ def main():
 
 
 if __name__ == "__main__":
+    input("I")
     main()
